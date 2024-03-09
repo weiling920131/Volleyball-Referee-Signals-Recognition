7415c4f1b2399a45747b148ff5ecf1d689a53b10
rebeccasun31 <rebeccasun31@gmail.com>
2024-03-10 02:16:28 +0800

    generate csv

--- generate.py
+++ generate.py
@@ -1,15 +1,25 @@
 import os
 import random
 import shutil
+import csv
 
 root_path = ".\\dataset"
 
 new_train_path = ".\\train"
 new_val_path = ".\\val"
 
+train_csv_path = ".\\train.csv"
+val_csv_path = ".\\val.csv"
+
+train_csv = open(train_csv_path, "w", newline='')
+val_csv = open(val_csv_path, "w", newline='')
+
+train_csv_writer = csv.writer(train_csv)
+val_csv_writer = csv.writer(val_csv)
+
 dirs = os.listdir(root_path)
-for dir in dirs:
-    dir_path = os.path.join(root_path, dir)
+for n in range(len(dirs)):
+    dir_path = os.path.join(root_path, dirs[n])
     if os.path.isdir(dir_path):
         imgs = os.listdir(dir_path)
         img_cnt = 0
@@ -30,8 +40,10 @@ for dir in dirs:
             if os.path.isdir(img_path):
                 if randIdx[i]:
                     new_path = os.path.join(new_train_path, img)
+                    train_csv_writer.writerow([img, dirs[n], n])
                 else:
                     new_path = os.path.join(new_val_path, img)
+                    val_csv_writer.writerow([img, dirs[n], n])
 
                 shutil.copytree(img_path, new_path)
                 i += 1
