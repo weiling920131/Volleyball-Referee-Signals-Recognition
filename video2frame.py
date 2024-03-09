ca1155f204fbb91fd73ac2bccc41433675a7e58d
weiling920131 <weiling920131@gmail.com>
2024-03-10 01:25:33 +0800

    dataset complete

--- video2frame.py
+++ video2frame.py
@@ -3,36 +3,27 @@ import cv2
 import os
 
 def extract_frames(video_path, output_dir):
-    # Open the video file
+
     video = cv2.VideoCapture(video_path)
-    
-    # Create the output directory if it doesn't exist
     os.makedirs(output_dir, exist_ok=True)
     
-    # Initialize frame counter
     cnt = 1
     frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT) / 30)
     if frame_count == 0:
         frame_count = 1
-    
-    # Read the first frame
+
     success, frame = video.read()
     
     last_frame = frame
     while success:
-        # Save the frame as an image
-        # if cnt < frame_count:
-        #     success, frame = video.read()
-        #     continue
         if cnt > 30:
             break
         output_path = os.path.join(output_dir, f"frame_{cnt}.jpg")
         cv2.imwrite(output_path, frame)
+        cnt += 1
         
-        # Read the next frame
         for _ in range(frame_count):
             success, frame = video.read()
-            cnt += 1
             if not success:
                 break
             last_frame = frame
@@ -43,7 +34,6 @@ def extract_frames(video_path, output_dir):
 
 
 if __name__ == "__main__":
-    # Example usage
     dataset_dir = "./dataset"
     for catagory in os.listdir(dataset_dir):
         cnt = 1
